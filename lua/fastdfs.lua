d81783f2f31fee0456a35c77f9a065f82907070b
hpxl <lvwl.cn@163.com>
2014-05-22 21:20:42 +0800

    update

--- lua/fastdfs.lua
+++ lua/fastdfs.lua
@@ -6,13 +6,13 @@ local function writefile(filename, info)
 end
 
 local function is_dir(sPath)
-	if type(sPath) ~= "string" then return false end
+    if type(sPath) ~= "string" then return false end
 
-	local response = os.execute( "cd " .. sPath )
-  	if response == 0 then
-    	return true
-  	end
-  	return false
+    local response = os.execute( "cd " .. sPath )
+    if response == 0 then
+        return true
+    end
+    return false
 end
 
 local file_exists = function(name)
@@ -22,10 +22,10 @@ end
 
 -- check image_dir exists
 local check_dir = function(dir)
-	if not is_dir(dir) then
-		os.execute("mkdir -p " .. dir)
-	end
-	return true
+    if not is_dir(dir) then
+        os.execute("mkdir -p " .. dir)
+    end
+    return true
 end
 
 local area = nil
@@ -33,46 +33,46 @@ local originalUri = ngx.var.uri;
 local originalFile = ngx.var.file;
 local index = string.find(ngx.var.uri, "([0-9]+)x([0-9]+)");  
 if index then 
-	originalUri = string.sub(ngx.var.uri, 0, index-2);  
-	area = string.sub(ngx.var.uri, index);  
-	index = string.find(area, "([.])");  
-	area = string.sub(area, 0, index-1);  
+    originalUri = string.sub(ngx.var.uri, 0, index-2);  
+    area = string.sub(ngx.var.uri, index);  
+    index = string.find(area, "([.])");  
+    area = string.sub(area, 0, index-1);  
 
-	local index = string.find(originalFile, "([0-9]+)x([0-9]+)");  
-	originalFile = string.sub(originalFile, 0, index-2)
+    local index = string.find(originalFile, "([0-9]+)x([0-9]+)");  
+    originalFile = string.sub(originalFile, 0, index-2)
 end
 
 -- check original file
 if not file_exists(originalFile) then
-	local fileid = string.sub(originalUri, 2);
-	-- main
-	local fastdfs = require('restyfastdfs')
-	local fdfs = fastdfs:new()
-	fdfs:set_tracker("192.168.1.113", 22122)
-	fdfs:set_timeout(1000)
-	fdfs:set_tracker_keepalive(0, 100)
-	fdfs:set_storage_keepalive(0, 100)
-	local data = fdfs:do_download(fileid)
-	if data then
-		-- check dir
-		check_dir(ngx.var.image_dir)
-		writefile(originalFile, data)
-	end
+    local fileid = string.sub(originalUri, 2);
+    -- main
+    local fastdfs = require('restyfastdfs')
+    local fdfs = fastdfs:new()
+    fdfs:set_tracker("192.168.1.113", 22122)
+    fdfs:set_timeout(1000)
+    fdfs:set_tracker_keepalive(0, 100)
+    fdfs:set_storage_keepalive(0, 100)
+    local data = fdfs:do_download(fileid)
+    if data then
+        -- check dir
+        check_dir(ngx.var.image_dir)
+        writefile(originalFile, data)
+    end
 end
 
 -- 创建缩略图
 local image_sizes = {"80x80", "800x600", "40x40", "60x60"};  
 function table.contains(table, element)  
-	for _, value in pairs(table) do  
-    	if value == element then
-        	return true  
+    for _, value in pairs(table) do  
+        if value == element then
+            return true  
         end  
     end  
     return false  
 end 
 
 if table.contains(image_sizes, area) then  
-	local command = "gm convert " .. originalFile  .. " -thumbnail " .. area .. " -background gray -gravity center -extent " .. area .. " " .. ngx.var.file;  
+    local command = "gm convert " .. originalFile  .. " -thumbnail " .. area .. " -background gray -gravity center -extent " .. area .. " " .. ngx.var.file;  
     os.execute(command);  
 end;
 
@@ -80,5 +80,5 @@ if file_exists(ngx.var.file) then
     --ngx.req.set_uri(ngx.var.uri, true);  
     ngx.exec(ngx.var.uri)
 else
-	ngx.exit(404)
+    ngx.exit(404)
 end
